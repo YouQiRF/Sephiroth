40a81348c807f210b6598fca78c99443574f62c0
YouQi <alexjohn2109a@gmail.com>
2023-03-18 18:58:14 +0800

    fix setting sound problem

--- Sephiroth3/Assets/Effect/SetSound/SetAudioVal.cs
+++ Sephiroth3/Assets/Effect/SetSound/SetAudioVal.cs
@@ -7,6 +7,10 @@ public class SetAudioVal : MonoBehaviour
 {
     public static float Sound_Val = 1;
     public Scrollbar bar;
+    void Start()
+    {
+        bar.value= Sound_Val;
+    }
     void Update()
     {
         Sound_Val = bar.value;
