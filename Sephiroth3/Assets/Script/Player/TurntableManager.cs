1be1a6b1a434d6ac4e549d660bf5ddccfc9afa6f
alals93vu6 <a0987915131@gmail.com>
2023-03-19 02:40:15 +0800

    Rotating Outer rig Part.1

--- Sephiroth3/Assets/Script/Player/TurntableManager.cs
+++ Sephiroth3/Assets/Script/Player/TurntableManager.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TurntableManager : MonoBehaviour
 {
     [Header("物件")]
     [SerializeField] public int SummonState;
+    [SerializeField] private float EndPoint;
     [SerializeField] public GameObject[] Summoner;
     [SerializeField] public GameObject[] SummonerTurntable;
+    [SerializeField] public GameObject turntable_outerring;
     //[SerializeField] public GameObject[] SummonerTurntable;
     [Header("Offset")]
     [SerializeField] private Vector3 SummonerOffset;
@@ -25,7 +28,11 @@ public class TurntableManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            EndPoint -= 25.5f;
+        }
+        RotatingOuterRing();
     }
 
     public void SimonCheck()
@@ -42,7 +49,12 @@ public class TurntableManager : MonoBehaviour
         Instantiate(Summoner[NowSummon], this.transform.position - SummonerOffset, new Quaternion(0,0,0,0));
         //Instantiate(SummonerTurntable[NowSummon], this.transform.position, this.transform.rotation);
     }
-    
+
+    private void RotatingOuterRing()
+    {
+        turntable_outerring.transform.rotation = Quaternion.Lerp(turntable_outerring.transform.rotation, quaternion.Euler(0,0,EndPoint), 0.2f);
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
