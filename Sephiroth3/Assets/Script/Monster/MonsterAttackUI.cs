73ce1ce4007761fbcaec2898db09290e70589d7f
alals93vu6 <a0987915131@gmail.com>
2023-03-27 17:21:06 +0800

    Attack UI Part.1

---
+++ Sephiroth3/Assets/Script/Monster/MonsterAttackUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterAttackUI : MonoBehaviour
+{
+    [SerializeField] public Image attackUI_Img;
+    [SerializeField] public Text attackUI_CD;
+    [SerializeField] public Sprite[] attackUI_ChangeImg;
+    [SerializeField] public Transform MonsterPos;
+    [SerializeField] private Vector3 imgOffset;
+    
+    [SerializeField] private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        attackUI_Img = this.transform.GetChild(0).GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            i++;
+        }
+        if (i ==attackUI_ChangeImg.Length)
+        {
+            i = 0;
+        }
+        attackUI_Img.sprite = attackUI_ChangeImg[i];
+        Follow();
+    }
+    
+    private void Follow()
+    {
+        Vector3 ScreenPos = Camera.main.WorldToScreenPoint(MonsterPos.position);
+        this.transform.position = ScreenPos + imgOffset;
+    }
+}
---
+++ Sephiroth3/Assets/Test/UIChangeIMG.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIChangeIMG : MonoBehaviour
+{
+    Image m_Image;
+    //Set this in the Inspector
+    public Sprite[] m_Sprite;
+    [SerializeField] private int i;
+
+    public Transform testOBJ;
+    [SerializeField]private Vector3 Offset;
+    
+
+    void Start()
+    {
+        i = 0;
+        //Fetch the Image from the GameObject
+        m_Image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        //Press space to change the Sprite of the Image
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            i++;
+        }
+        if (i == m_Sprite.Length)
+        {
+            i = 0;
+        }
+        m_Image.sprite = m_Sprite[i];
+        Follow();
+    }
+
+    private void Follow()
+    {
+        Vector3 ScreenPos = Camera.main.WorldToScreenPoint(testOBJ.position);
+        this.transform.position = ScreenPos + Offset;
+    }
+}
