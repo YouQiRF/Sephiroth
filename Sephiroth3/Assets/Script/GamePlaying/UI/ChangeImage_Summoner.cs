6c7b7d5cf74c44eea119299c182c8a4805515042
alals93vu6 <a0987915131@gmail.com>
2023-04-04 00:37:01 +0800

    Player Summoner Avatar Change

--- Sephiroth3/Assets/Script/GamePlaying/UI/ChangeImage_Summoner.cs
+++ Sephiroth3/Assets/Script/GamePlaying/UI/ChangeImage_Summoner.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Project.AvatarData;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeImage_Summoner : MonoBehaviour
 {
+    [SerializeField] private bool isSummonerA;
+    [SerializeField] private Image summonerImage;
+    [SerializeField] private AvatarData Avatar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        summonerImage = GetComponent<Image>();
+        ChangeIMG();
     }
 
-    // Update is called once per frame
-    void Update()
+    private async void ChangeIMG()
     {
-        
+        await Task.Delay(150);
+        if (isSummonerA)
+        {
+            summonerImage.sprite = Avatar.avatarImage[GameObject.Find("TheSummonerA").GetComponent<SummonerManager>().thisNumber];
+        }
+        else
+        {
+            summonerImage.sprite = Avatar.avatarImage[GameObject.Find("TheSummonerB").GetComponent<SummonerManager>().thisNumber];
+        }
     }
 }
