6463f03f2833d15107c7a3d0a9dd4912ea12cac0
alals93vu6 <a0987915131@gmail.com>
2023-04-03 23:04:46 +0800

    Monster HP Lerp

---
+++ Sephiroth3/Assets/Script/GamePlaying/UI/MonsterHPUIDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterHPUIDisplay : MonoBehaviour
+{
+    [SerializeField] private int ThisNumber;
+    //[SerializeField] private float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LocationManager.instance.MonsterLocation[ThisNumber] == null)
+        {
+            this.transform.position = Vector3.Lerp(transform.position,
+                new Vector3(this.transform.position.x, 1200, this.transform.position.z), 0.02f);
+        }
+        else
+        {
+            this.transform.position = Vector3.Lerp(transform.position,
+                new Vector3(this.transform.position.x, 850, this.transform.position.z), 0.05f);
+        }
+    }
+}
