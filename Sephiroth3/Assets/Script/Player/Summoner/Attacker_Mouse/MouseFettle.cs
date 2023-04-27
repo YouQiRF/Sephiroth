81ca4fa08245902ff4b7a9aab1fc0a6715988146
alals93vu6 <a0987915131@gmail.com>
2023-04-28 01:50:11 +0800

    Add Summoner_Mouse Part.4

--- Sephiroth3/Assets/Script/Player/Summoner/Attacker_Mouse/MouseFettle.cs
+++ Sephiroth3/Assets/Script/Player/Summoner/Attacker_Mouse/MouseFettle.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class MouseFettle : FettleGeneric
@@ -12,21 +13,27 @@ public class MouseFettle : FettleGeneric
         base.OnStart();
     }
 
-    public override void OnSetLocation()
+    public async override void OnSetLocation()
     {
+        var MouseAN = GameObject.Find("Mouse_Animation").GetComponent<Animator>();
+        var AttackMove = GameObject.Find("MouseAN").GetComponent<AttackDisPlay_Player>();
         BeforLocaTion = StatyLocation;
         EnemyDetected();
         base.OnSetLocation();
+        MouseAN.Play("mouse_attack");
+        await Task.Delay(600);
         if (BeforLocaTion > StatyLocation)
         {
             LocationManager.instance.OtherAttackDetected(8,hitEnemy);
-            Debug.Log("AttackFront");
+            //Debug.Log("AttackFront");
         }
         else
         {
             LocationManager.instance.EnemyOnAttackDetected(8);
-            Debug.Log("AttackBehind");
+            //Debug.Log("AttackBehind");
         }
+        AttackMove.OnAttackDisPlay();
+        Creat_Effect_Player.instance.Creat(Creat_Effect_Player.instance.Shake_Camera_M,Creat_Effect_Player.instance.Buff_Hit_pos[0]);
     }
     
     private void EnemyDetected()
--- Sephiroth3/Assets/Script/Player/Summoner/Attacker_Mouse/Mouse_Turntable/Event_MouseAttack.cs
+++ Sephiroth3/Assets/Script/Player/Summoner/Attacker_Mouse/Mouse_Turntable/Event_MouseAttack.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Event_MouseAttack : TurntableGeneric
 {
-    public override void OnPointed()
+    public async override void OnPointed()
     {
         //var AttackNumber = FindObjectOfType<LocationManager>();
         //var AttackAN = GameObject.Find("WolfAN").GetComponent<AttackDisPlay_Player>();
         var AttackAN = GameObject.Find("MouseAN").GetComponent<AttackDisPlay_Player>();
+        var MouseAN = GameObject.Find("Mouse_Animation").GetComponent<Animator>();
+        MouseAN.Play("mouse_attack");
+        await Task.Delay(600);
         LocationManager.instance.EnemyOnAttackDetected(1);
         Creat_Effect_Player.instance.Creat(Creat_Effect_Player.instance.Shake_Camera_M,Creat_Effect_Player.instance.Buff_Hit_pos[0]);
         AttackAN.OnAttackDisPlay();//Debug.Log("Attackwolf");
+        Creat_Effect_Player.instance.Creat(Creat_Effect_Player.instance.Shake_Camera_M,Creat_Effect_Player.instance.Buff_Hit_pos[0]);
     }
 }
