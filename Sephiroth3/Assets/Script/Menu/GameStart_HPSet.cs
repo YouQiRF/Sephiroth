8d07d6f62946a69f73676f5ae1511f34fe5cbec2
alals93vu6 <a0987915131@gmail.com>
2023-05-17 02:19:44 +0800

    Manual Reset

--- Sephiroth3/Assets/Script/Menu/GameStart_HPSet.cs
+++ Sephiroth3/Assets/Script/Menu/GameStart_HPSet.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Project.PlayerHpData;
 using UnityEngine;
@@ -7,12 +8,22 @@ public class GameStart_HPSet : MonoBehaviour
 {
     [SerializeField] private HpData[] _hpData;
     // Start is called before the first frame update
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerPrefs.SetInt("NeedReset",1);
+            Debug.Log(PlayerPrefs.GetInt("NeedReset"));
+        }
+    }
+
     public void OnStartSet()
     {
-        for (int i = 0; i < _hpData.Length; i++)
-        {
-            _hpData[0].NowHP = 999;
-        }
+        _hpData[0].NowHP = 111;
+        _hpData[1].NowHP = 222;
+        _hpData[2].NowHP = 333;
+        _hpData[3].NowHP = 444;
         PlayerPrefs.SetInt("SummonerA",1);
         PlayerPrefs.SetInt("SummonerB",0);
     }
